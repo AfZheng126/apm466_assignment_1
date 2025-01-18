fixed calculation of matrices and added final data

assignment_1.py
@@ -76,7 +76,7 @@ def interpolate_rate(d, x):
         interpolated_value = yl + (x - xl) * (yu - yl) / (xu - xl)
         return interpolated_value
 
-def create_sorted_bonds_by_maturity():
+def create_sorted_bonds_by_maturity(file_name):
     # If you want to read only a subset of sheets
     # xls = pd.ExcelFile('bond_prices.xlsx')
     # sheet_names = xls.sheet_names
@@ -84,14 +84,14 @@ def create_sorted_bonds_by_maturity():
     # desired_sheets = [sheet for sheet in sheet_names if sheet.startswith('Desired')]
     # dfs = pd.read_excel('bond_prices.xlsx', sheet_name=desired_sheets, usecols = ['Coupon', 'Maturity Date', 'Bid', 'Ask', 'Years until Maturity'])
 
-    dfs = pd.read_excel('bond_prices.xlsx', sheet_name=None, usecols = ['Coupon', 'Maturity Date', 'Bid', 'Ask', 'Years until Maturity'])
+    dfs = pd.read_excel(file_name, sheet_name=None, usecols = ['Coupon', 'Maturity Date', 'Price', 'Years until Maturity'])
     
     for sheet_name, df in dfs.items():
         print(f"Processing sheet: {sheet_name}")
         # get all the required information for the bonds
         df['Maturity Date'] = pd.to_datetime(df['Maturity Date'])
         df['Days since last coupon payment'] = df['Maturity Date'].apply(calculate_days_since_last_coupon_payment)
-        df['Dirty price'] = df.apply(lambda row: calculate_dirty_prices(row['Ask'], row['Days since last coupon payment'], row['Coupon']), axis= 1)
+        df['Dirty price'] = df.apply(lambda row: calculate_dirty_prices(row['Price'], row['Days since last coupon payment'], row['Coupon']), axis= 1)
         # print(df[['Dirty price', 'Years until Maturity']])
 
     return dfs
@@ -182,6 +182,17 @@ def calculate_ytm_curve(bonds):
 
     return all_ytm_vals
 
+# calculate the future price of a zero coupon bond
+def calculate_future_price_zero_bonds(future_time, years_until_maturity, spot_rates, coupon_rate):
+    if years_until_maturity in spot_rates:
+            r = spot_rates[years_until_maturity]
+    else:
+        r = interpolate_rate(spot_rates, years_until_maturity)
+    future_price = np.exp(r * (years_until_maturity - future_time)) # no need for the 100 as it gets cancelled out in the log anyways
+    return future_price
+
+
+# calculate the future price of a coupon bond
 def calculate_future_price(future_time, years_until_maturity, spot_rates, coupon_rate):
     number_of_coupon_payments = int(np.floor((years_until_maturity - future_time) * 2))
     future_price = 0
@@ -222,7 +233,8 @@ def calculate_forward_rate(all_spot_rates, all_bonds):
             if row['Dirty price'] == "-" or years_until_maturity < 1 or (years_until_maturity in maturities):
                 continue
             maturities.append(years_until_maturity)
-            future_prices.append(np.log(calculate_future_price(1, years_until_maturity, spot_rates, row['Coupon'])))
+            # future_prices.append(np.log(calculate_future_price(1, years_until_maturity, spot_rates, row['Coupon'])))
+            future_prices.append(np.log(calculate_future_price_zero_bonds(1, years_until_maturity, spot_rates, row['Coupon'])))
         
         # print(f'maturities: {maturities}')
         # print(f'prices: {future_prices}')
@@ -244,31 +256,86 @@ def calculate_forward_rate(all_spot_rates, all_bonds):
     return all_forward_rates
 
 # calculate covariance matricies
-def calculate_covariance_matricies(all_yield_rates, all_forward_rates):
-    # create the 5 random vectors X_i, each with 9 entries
+def calculate_covariance_matricies_for_yields(all_rates, num_variables):
+    # create the 5 random vectors X_i, 
+    dates = list(all_rates.keys())
+    num_rows = len(dates) - 1
 
     vectors = []
-    for i in range(5):
-        vectors.append(np.zeros(9))
-    dates = list(all_yield_rates.keys())
+    for i in range(num_variables):
+        vectors.append(np.zeros((num_rows, 1)))
+
     # vector for yield rates
-    for j in range(9): # each column is a date in our 10 day data collection period
-        today_yield_rates = all_yield_rates[dates[j]]
-        tomorrow_yield_rates = all_yield_rates[dates[j+1]]
-        for i in range(5): 
-            years_until_maturity = float(i + 1)
-            if years_until_maturity in today_yield_rates:
-                today_rate = today_yield_rates[years_until_maturity]
+    for j in range(num_rows): # each row is a date in our data collection period
+        today_rates = all_rates[dates[j]]
+        tomorrow_rates = all_rates[dates[j + 1]]
+        for i in range(num_variables): 
+            years_until_maturity = float(i + 1) 
+            # the 5 variables are the 1, 2, 3, 4, 5 year rates for ytm
+            # for forward rates it is the 1-1, 1-2, 1-3, 1-4, 1-5 year rates
+
+            if years_until_maturity in today_rates:
+                today_rate = today_rates[years_until_maturity]
             else:
-                today_rate = interpolate_rate(today_yield_rates, years_until_maturity)
-            if years_until_maturity in tomorrow_yield_rates:
-                tomorrow_rate = tomorrow_yield_rates[years_until_maturity]
+                today_rate = interpolate_rate(today_rates, years_until_maturity)
+            if years_until_maturity in tomorrow_rates:
+                tomorrow_rate = tomorrow_rates[years_until_maturity]
             else:
-                tomorrow_rate = interpolate_rate(tomorrow_yield_rates, years_until_maturity)
+                tomorrow_rate = interpolate_rate(tomorrow_rates, years_until_maturity)
             vectors[i][j] = np.log(tomorrow_rate / today_rate)
 
     # create the covariance matrix
-    data = np.vstack([vectors[0], vectors[1], vectors[2], vectors[3], vectors[4]])
+    data = np.hstack(vectors)
+    covariance_matrix = np.cov(data, rowvar=False)
+    print(f'Covariance matrix: {covariance_matrix}')
+
+    # calculate the Eigenvalues and Eigenvectors
+    eigenvalues, eigenvectors = np.linalg.eig(covariance_matrix)
+    print("Eigenvalues:", eigenvalues)
+    print("Eigenvectors:\n", eigenvectors)
+
+def calculate_covariance_matricies_for_forward_rates(all_rates, num_variables):
+    # create the 5 random vectors X_i, 
+    dates = list(all_rates.keys())
+    num_rows = len(dates) - 1
+
+    vectors = []
+    for i in range(num_variables):
+        vectors.append(np.zeros((num_rows, 1)))
+
+    # vector for yield rates
+    for j in range(num_rows): # each row is a date in our data collection period
+        today_rates = all_rates[dates[j]]
+        tomorrow_rates = all_rates[dates[j + 1]]
+        for i in range(num_variables): 
+            years_until_maturity = float(i + 1) 
+            # for forward rates it is the 1-1, 1-2, 1-3, 1-4, 1-5 year rates
+
+            if years_until_maturity in today_rates:
+                today_rate = today_rates[years_until_maturity]
+            else:
+                # if years_until_maturity is 1, then there is no lower bound so just set it to the smallest year in today_rates
+                if years_until_maturity == 1:
+                    smallest_key = min(today_rates)
+                    today_rate = today_rates[smallest_key]
+                else:
+                    today_rate = interpolate_rate(today_rates, years_until_maturity)
+            if years_until_maturity in tomorrow_rates:
+                tomorrow_rate = tomorrow_rates[years_until_maturity]
+            else:
+                if years_until_maturity == 1:
+                    smallest_key = min(tomorrow_rates)
+                    # print(f"smallest key: {smallest_key}")
+                    tomorrow_rate = tomorrow_rates[smallest_key]
+                else:
+                    tomorrow_rate = interpolate_rate(tomorrow_rates, years_until_maturity)
+            # print(f"date: {years_until_maturity}, today rate: {today_rate}, tomorrow rate: {tomorrow_rate}, value: {np.log(tomorrow_rate / today_rate)}")
+            vectors[i][j] = np.log(tomorrow_rate / today_rate)
+
+    # print(vectors)
+
+    # create the covariance matrix
+    data = np.hstack(vectors)
     covariance_matrix = np.cov(data, rowvar=False)
     print(f'Covariance matrix: {covariance_matrix}')
 
@@ -347,13 +414,22 @@ def plot_difference(yield_rates, ytm_rates):
     plt.legend(loc='upper right')
 
 # get the data
-sorted_bonds = create_sorted_bonds_by_maturity()
+sorted_bonds = create_sorted_bonds_by_maturity('bond_prices_chosen_bonds.xlsx')
 spot_rates = calculate_all_spot_rates(sorted_bonds)
 yield_rates = calculate_ytm_curve(sorted_bonds)
 forward_rates = calculate_forward_rate(spot_rates, sorted_bonds)
 
-# plot the data
+# print(forward_rates)
 
+# get the eigenvalues
+print("covariance for ytm rates")
+calculate_covariance_matricies_for_yields(yield_rates, 5)
+
+print("covariance for forward rates")
+calculate_covariance_matricies_for_forward_rates(forward_rates, 5)
+
+
+# plot the data
 plt.figure()
 plot_spot_curve(spot_rates)
 plt.figure()